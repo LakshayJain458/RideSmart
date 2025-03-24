085c55639c9995ab448a5eb451eaa75d63dc3353
LakshayJain458 <aalvpbjain93@gmail.com>
2025-03-24 21:42:39 +0530

    Initial commit

---
+++ src/main/java/org/example/ridesmart/RideSmartApplication.java
@@ -0,0 +1,13 @@
+package org.example.ridesmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideSmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ridesmart/RideSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ridesmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
